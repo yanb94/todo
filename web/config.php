6b2deb2cf816fb7bd05eb98737b533168efd8247
Yanb <yann-elias@wanadoo.fr>
2018-03-19 16:07:51 +0100

    Fix bugs with PHPunit Process

--- web/config.php
+++ web/config.php
@@ -367,11 +367,11 @@ $hasMinorProblems = (bool) count($minorProblems);
                             ready to run Symfony applications.
                         </p>
 
-                        <?php if ($hasMajorProblems) : ?>
+                        <?php if ($hasMajorProblems): ?>
                             <h2 class="ko">Major problems</h2>
                             <p>Major problems have been detected and <strong>must</strong> be fixed before continuing:</p>
                             <ol>
-                                <?php foreach ($majorProblems as $problem) : ?>
+                                <?php foreach ($majorProblems as $problem): ?>
                                     <li><?php echo $problem->getTestMessage() ?>
                                         <p class="help"><em><?php echo $problem->getHelpHtml() ?></em></p>
                                     </li>
@@ -379,18 +379,14 @@ $hasMinorProblems = (bool) count($minorProblems);
                             </ol>
                         <?php endif; ?>
 
-                        <?php if ($hasMinorProblems) : ?>
+                        <?php if ($hasMinorProblems): ?>
                             <h2>Recommendations</h2>
                             <p>
-                                <?php if ($hasMajorProblems) :
-?>Additionally, to<?php
-                                else :
-?>To<?php
-                                endif; ?> enhance your Symfony experience,
+                                <?php if ($hasMajorProblems): ?>Additionally, to<?php else: ?>To<?php endif; ?> enhance your Symfony experience,
                                 it’s recommended that you fix the following:
                             </p>
                             <ol>
-                                <?php foreach ($minorProblems as $problem) : ?>
+                                <?php foreach ($minorProblems as $problem): ?>
                                     <li><?php echo $problem->getTestMessage() ?>
                                         <p class="help"><em><?php echo $problem->getHelpHtml() ?></em></p>
                                     </li>
@@ -398,22 +394,22 @@ $hasMinorProblems = (bool) count($minorProblems);
                             </ol>
                         <?php endif; ?>
 
-                        <?php if ($symfonyRequirements->hasPhpIniConfigIssue()) : ?>
+                        <?php if ($symfonyRequirements->hasPhpIniConfigIssue()): ?>
                             <p id="phpini">*
-                                <?php if ($symfonyRequirements->getPhpIniConfigPath()) : ?>
+                                <?php if ($symfonyRequirements->getPhpIniConfigPath()): ?>
                                     Changes to the <strong>php.ini</strong> file must be done in "<strong><?php echo $symfonyRequirements->getPhpIniConfigPath() ?></strong>".
-                                <?php else : ?>
+                                <?php else: ?>
                                     To change settings, create a "<strong>php.ini</strong>".
                                 <?php endif; ?>
                             </p>
                         <?php endif; ?>
 
-                        <?php if (!$hasMajorProblems && !$hasMinorProblems) : ?>
+                        <?php if (!$hasMajorProblems && !$hasMinorProblems): ?>
                             <p class="ok">All checks passed successfully. Your system is ready to run Symfony applications.</p>
                         <?php endif; ?>
 
                         <ul class="symfony-install-continue">
-                            <?php if ($hasMajorProblems || $hasMinorProblems) : ?>
+                            <?php if ($hasMajorProblems || $hasMinorProblems): ?>
                                 <li><a href="config.php">Re-check configuration</a></li>
                             <?php endif; ?>
                         </ul>
